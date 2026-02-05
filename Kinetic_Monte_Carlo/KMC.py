ffb0a06784027a8e8946752bfb4224f4ba48edb9
LucaaB <134833089+BLucaa@users.noreply.github.com>
2026-02-05 11:53:10 +0100

    KMC for Hydrogen moving in O lattice implemented

--- Kinetic_Monte_Carlo/KMC.py
+++ Kinetic_Monte_Carlo/KMC.py
@@ -2,6 +2,7 @@
 #%%
 import numpy as np 
 import numba 
+import math
 #%%Kinetic Monte Carlo
 
 @numba.jit(nopython=True)
@@ -78,35 +79,77 @@ def KMC2(position_0, gamma_1, gamma_2, a,b ,N):
     position = position_0.copy()
     gamma = gamma_1 + gamma_2
     d_1 = a - 2*b
-    pos_array = []
+    d_2 = math.sqrt(2)*b
+    pos_array = pos_array = np.zeros((N, 3), dtype=np.float64)
+    
     for i in range(N):
         x_1 = np.random.rand(1)[0]
-        x_2 = np.random.randint(0,4,1)[0]
-        if x_1*gamma > 0 and x_1*gamma < gamma_1 : 
+        x_2 = np.random.randint(0,2,1)[0]
+        if x_1*gamma > 0 and x_1*gamma < gamma_1: 
             #hopping from O to another O 
-            
-            if x_2 <= 1 :
-                if position[2] == 
+            if position[2]==1 :
                 position[0] = position[0] + d_1
-                position[2] =
-            elif :
-                
+                position[2] = 3
+            elif position[2]==3 : 
+                position[0] = position[0] - d_1
+                position[2] = 1
+            elif position[2] == 0 :
+                position[1] = position[1] + d_1
+                position[2] = 2
             else :
+                position[1] = position[1] - d_1
+                position[2] = 0
                 
-        elif gamma*x_1 > gamma_1 : 
-            if x_2 == 0 :
-                position[1] = position[1] - 1
-                position[0] = position[0] + 1
-            elif x_2 == 1 :
-                position[1] = position[1] + 1
-                position[0] = position[0] - 1
-            elif x_2 == 2 :
-                position[1] = position[1] - 1
-                position[0] = position[0] - 1
-            else :
-                position[1] = position[1] + 1
-                position[0] = position[0] + 1
-        pos_array.append(position.copy())
+        elif  gamma*x_1 > gamma_1 : 
+            #
+            if position[2] == 0: 
+                position[1] = position[1] - b 
+                if x_2 == 0 : 
+                    position[0] = position[0] + b 
+                    position[2] = 1
+                else : 
+                    position[0] = position[0] - b
+                    position[2] = 3
+                    
+            elif position[2] == 1: 
+                position[0] = position[0] - b 
+                if x_2 == 0 : 
+                    position[1] = position[1] + b
+                    position[2] = 0 
+                else : 
+                    position[1] = position[1] - b
+                    position[2] = 2
+            
+            elif position[2] == 2: 
+                position[1] = position[1] + b 
+                if x_2 == 0 : 
+                    position[0] = position[0] + b
+                    position[2] = 1
+                else : 
+                    position[0] = position[0] - b
+                    position[2] = 3
+                    
+            elif position[2] == 3: 
+                position[0] = position[0] + b 
+                if x_2 == 0 : 
+                    position[1] = position[1] + b 
+                    position[2] = 0
+                else : 
+                    position[1] = position[1] - b
+                    position[2] = 0
+            
+        pos_array[i, 0] = position[0]
+        pos_array[i, 1] = position[1]
+        pos_array[i, 2] = position[2]
         
-    
     return pos_array
+
+#%%test KMC2
+position_0 =[0,0,0]
+gamma_1 =1/2
+gamma_2 = 1/2
+a=1
+b=1
+N=200
+a= np.array(KMC2(position_0, gamma_1, gamma_2, a,b ,N))
+print(a) 
